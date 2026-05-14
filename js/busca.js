a66ad45d0ae4d9e8cd6fda891d154ea8bcbf4df6
Newton Brito Santos <newton62574186@edu.df.senac.br>
2026-05-14 17:32:27 -0300

    14/05

--- js/busca.js
+++ js/busca.js
@@ -1,3 +1,34 @@
-const forBusca = document.querySelector('.busca');
-const campoBusca = document.querySelector('.busca__botao')
+const formBusca = document.querySelector('.busca');
+const campoBusca = document.querySelector('.busca__campo')
 const itensCatalogo = document.querySelectorAll('.bordados')
+
+//Verificar se o formulario de busca existe na página
+
+if(formBusca) {
+    formBusca.addEventListener('submit',function(event){
+    event.preventDefault();
+})
+}
+
+//Agora vamos verificar o texto que foi digitado
+
+campoBusca.addEventListener('input', function(){
+
+    //Converter o texto para minúsculo para facilitar a comparação
+    const termoBusca = campoBusca.value.toLowerCase();
+    
+    //Vamos verificar todas as seções de bordados
+    itensCatalogo.forEach(function(item){
+
+        //Pegar o elemento que tem o texto do bordado
+        const titulo = item.querySelector('h3').innerText.toLowerCase();
+
+        //verificar se o termo de busca é igual ao título
+        if(titulo.includes(termoBusca)){
+            item.style.display = 'block'; // Mostrar o item
+
+        }else{
+            item.style.display = 'none'; // Ocultar o item
+        }
+    })
+})
