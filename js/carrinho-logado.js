57ab5a47812097703c8c8d7de0491be5dcfe0d8b
Newton Brito Santos <newton62574186@edu.df.senac.br>
2026-06-03 15:00:19 -0300

    03/06

--- js/carrinho-logado.js
+++ js/carrinho-logado.js
@@ -1,72 +1,72 @@
-//Vamos verificar se o usuario está logado
+//Vamos verificar se o usuário está logado
 const usuarioLogado = localStorage.getItem('usuarioCadastro')
 
 //Se não estiver logado é direcionado para a página de login
-if (!usuarioLogado) {
-    document.innerHTML =
-        '<div class="container text-center mt-5">' +
+if(!usuarioLogado){
+    document.innerHTML = 
+    '<div class="container text-center mt-5">'+
         '<h2 class="text-danger"><i class="fa-solid fa-lock"></i> Acesso Negado</h2>' +
-        '<p class="mb-4">Você precisa estar logado.</p>' +
+        '<p class="mb-4">Você precisa estar logado.</p>'+
         '<p class="text-muted">Aguarde...</p>' +
-        '</div>'
+    '</div>'
 
     setTimeout(() => {
         window.location.href = 'login.html'
     }, 2500)
-
-} else {
-
+    
+}else {
+    
     //Reunindo as informações do usuário logado
     const carrinho = JSON.parse(localStorage.getItem('itemCarrinho')) || []
     const listaProdutosHTML = document.getElementById('lista-produtos')
     const textoTotal = document.getElementById('texto-total')
     let valorTotal = 0
     let textoPedidoPorEmail = 'Olá, gostaria de fazer um pedido:\n\n'
-
+    
     //Vamos verificar se o carrinho está vazio
-    if (carrinho.length === 0) {
-        listaProdutosHTML.innerHTML =
-            '<tr>' +
-            '<td colspan="2" class=text-center>Seu carrinho está vazio.</td>' +
-            '</tr>'
-    } else {
+    if(carrinho.length === 0){
+        listaProdutosHTML.innerHTML = 
+        '<tr>'+
+            '<td colspan="2" class="text-center">Seu carrinho está vazio.</td>'+
+        '</tr>'
+    }else{
         //Cada item do carrinho é exibido na tabela
-        carrinho.forEach(function (item) {
+        carrinho.forEach(function(item){
             listaProdutosHTML.innerHTML +=
-                '<tr>' +
-                '<td>' + item.nome + '</td>' +
-                '<td>' + item.preco + '</td>' +
-                '</tr>'
+            '<tr>'+
+                '<td>' + item.nome + '</td>'+
+                '<td>R$ ' + item.preco + ' </td>'+
+            '</tr>'
 
             //Somar o total
             valorTotal += parseFloat(item.preco)
 
             //Colocando os itens no e-mail
             textoPedidoPorEmail += '- ' + item.nome + ': R$ ' + item.preco + '\n'
-
         })
+
         textoTotal.innerText = 'Total: R$ ' + valorTotal
         textoPedidoPorEmail += '\nValor Total: R$ ' + valorTotal
-   
+    
 
     //finalizar o pedido por e-mail
     const btnFinalizar = document.getElementById('btn-finalizar')
-    
-    btnFinalizarFinalizar.addEventListener('click', function(){
+
+    btnFinalizar.addEventListener('click', function(){
         event.preventDefault()
 
         if(carrinho.length === 0){
             const textOriginal = btnFinalizar.innerText
-            btnFinalizar.innertext = 'O Carrinho está vazinho!'
+            btnFinalizar.innerText = 'O Carrinho está vazio!'
             btnFinalizar.classList.replace('btn-success', 'btn-danger')
 
             setTimeout(() => {
                 btnFinalizar.innerText = textOriginal
                 btnFinalizar.classList.replace('btn-danger', 'btn-success')
             }, 2500)
-            
+
             return
-        }
+        } 
 
         btnFinalizar.innerText = 'Preparando pedido...'
         btnFinalizar.classList.replace('btn-success', 'btn-secondary')
@@ -79,13 +79,15 @@ if (!usuarioLogado) {
 
         const linkEmail = 'https://mail.google.com/mail/?view=cm&fs=1&to=${emailTiaSonia}&su=${encodeURIComponent(assunto)}&body=${encodeURIComponent(textoPedidoPorEmail)}'
 
-        window.open(linkGmail, '_blank')
+        window.open(linkEmail, '_blank')
 
-        localStorage.removeItem('itemcarrinho')
+        localStorage.removeItem('itemCarrinho')
 
         setTimeout(() => {
             window.location.href = 'index.html'
         }, 1500)
-    })
-   } 
-}
+        })
+    }
+}      
+
+
